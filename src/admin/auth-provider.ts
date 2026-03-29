484f77cadc91b15c90fe1e305b6bd20a7e31b5d0
Mihail <mihailsorin58@gmail.com>
2026-03-29 16:57:34 +0500

    add hardcoded auth, dev only

--- src/admin/auth-provider.ts
+++ src/admin/auth-provider.ts
@@ -3,13 +3,12 @@ import { DefaultAuthProvider } from 'adminjs';
 import componentLoader from './component-loader.js';
 import { DEFAULT_ADMIN } from './constants.js';
 
-/**
- * Make sure to modify "authenticate" to be a proper authentication method
- */
+// hardcoded login and password, ts will not be in prod
+
 const provider = new DefaultAuthProvider({
   componentLoader,
   authenticate: async ({ email, password }) => {
-    if (email === DEFAULT_ADMIN.email) {
+    if (email === DEFAULT_ADMIN.email && password === DEFAULT_ADMIN.password) {
       return { email };
     }
 
